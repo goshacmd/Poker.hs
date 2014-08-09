f4ef15648da7ed39b264410d92c02598c16590bc
Gosha Arinich <me@goshakkk.name>
2014-08-09 16:32:07 +0300

    shuffle function

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,19 @@
-module Utils where
+module Utils (tuplify5,
+              untuplify5,
+              tuplify4,
+              consec',
+              removeTwo,
+              joinF,
+              tupF)
+  where
 
 import Data.List
+import Data.Array.ST
+import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Random
+import System.Random
+import GHC.Arr
 
 tuplify5 :: [a] -> (a, a, a, a, a)
 tuplify5 (a:b:c:d:e:_) = (a, b, c, d, e)
@@ -24,3 +37,17 @@ joinF f x = x : f x
 
 tupF :: (a -> b) -> a -> (a, b)
 tupF f x = (x, f x)
+
+shuffle :: RandomGen g => [a] -> Rand g [a]
+shuffle xs = do
+  let l = length xs
+  rands <- take l `fmap` getRandomRs (0, l-1)
+  let ar = runSTArray $ do
+      ar <- thawSTArray $ listArray (0, l-1) xs
+      forM_ (zip [0..(l-1)] rands) $ \(i, j) -> do
+        vi <- readSTArray ar i
+        vj <- readSTArray ar j
+        writeSTArray ar j vi
+        writeSTArray ar i vj
+      return ar
+  return (elems ar)
